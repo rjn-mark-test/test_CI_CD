07da66ab0b191ccdacba6dde44b0fdb83e15f67d
mark <156172870+rjn-mark-test@users.noreply.github.com>
2024-01-10 14:50:24 +0530

    Added post request for sub

--- app.js
+++ app.js
@@ -15,4 +15,12 @@ app.post("/addup", (req, res) => {
    res.send({ finalSum: finalSum });
 });
 
+app.post("/subtract", (req, res) => {
+   let a = parseFloat(req.body.num1);
+   let b = parseFloat(req.body.num2);
+   let finalSum = a - b;
+
+   res.send({ finalSub: finalSub });
+});
+
 module.exports = app; // Export the Express app

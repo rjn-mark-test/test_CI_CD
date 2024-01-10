62ee7405b14af88afada6203b6bd3adfb1fc82f0
mark <156172870+rjn-mark-test@users.noreply.github.com>
2024-01-10 15:59:26 +0530

    deleted post_req add

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post("/addup", (req, res) => {
-   let a = parseFloat(req.body.num1);
-   let b = parseFloat(req.body.num2);
-   let finalSum = a + b;
 
-   res.send({ finalSum: finalSum });
-});
 
 app.post("/sub", (req, res) => {
    let a = parseFloat(req.body.num1);
